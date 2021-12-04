25ab8a7c1f997c0bebb3493e1bd94235ac593d28
Preface_Ehsan <preface_ehsan@Preface-Ehsans-MacBook-Air.local>
2021-12-05 00:32:29 +0800

    add the keyboard and some styling to it

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render() {
+        const headerStyle ={
+            backgroundColor: '#7c9bcc',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: '0px',
+            border: '10px solid',
+            padding: '50px'
+            
+
+
+        }
+        return (
+            <h1 style={headerStyle}> This is the Heading</h1>
+        )
+    }
+}
+
+export default Header;
