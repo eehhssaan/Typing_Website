8cd777b2ecb6c4dd50b59b80ce27aec49812be40
Preface_Ehsan <preface_ehsan@Preface-Ehsans-MacBook-Air.local>
2021-12-09 19:02:23 +0800

    created criteria to create paragraph for testing randomly

--- src/Components/Paragraph.js
+++ src/Components/Paragraph.js
@@ -1,26 +1,34 @@
 import React, {Component} from 'react';
-import {text} from '../static/words.txt'
+import words from './para.js'
+
 
 class KeyBoard extends Component {
-
     render() {
+        function choose_words(){
+            const list_of_words = Array(words);
+            let choosen_words_paragraph = " ";
+            let choosen_words_array = [];
+
+            for (let i = 0; i < 20; i++) {
+                let element = Math.floor(Math.random()*1000)
+                choosen_words_array.push(list_of_words[0][element]+ " ") ;
+
+            }
+            return choosen_words_array
+        }
+        let words_returned = choose_words()
         return (
             <>
                 <div>
-                    <h1 style={{
-                        border: '1px solid'
-                    }}
-                    > 
-                    This is a long paragraph.
-                    </h1>
+                    <p style={{border: '1px solid', fontSize: '40px'}} > 
+                    {words_returned}
+                    </p>
                 </div>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                }}>
-                    <h1 style={{ border: '1px solid', margin: '20px',padding: '10px' }}> Previous Word</h1>
-                    <h1 style={{border: '1px solid', margin: '20px' ,padding: '10px' }}> Current Word</h1>
-                    <h1 style={{ border: '1px solid', margin: '20px' ,padding: '10px' }}> Next Word</h1>
+                <div style={{ display: 'flex',justifyContent: 'center' }}>
+                    
+                    <h1 style={{ border: '1px solid', margin: '20px',padding: '10px' }}> Previous word</h1>
+                    <h1 style={{border: '1px solid', margin: '20px' ,padding: '10px' }}> {words_returned[0]}</h1>
+                    <h1 style={{ border: '1px solid', margin: '20px' ,padding: '10px' }}> {words_returned[1]}</h1>
                 </div>
                 
             </>
